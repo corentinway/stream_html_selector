6f96e2ca69dab256383a1853774d58c614179907
Corentin Jechoux <corentinway@gmail.com>
2021-05-16 23:51:15 +0200

    foo bar

--- src/tag.rs
+++ src/tag.rs
@@ -3,8 +3,8 @@ use std::collections::HashMap;
 pub struct Tag {
     pub name: String,
     id: Option<String>,
-    classes: Option<Vec<String>>,
-    attributes: Option<HashMap<String, String>>,
+    classes: Option<String>,
+    attributes: HashMap<String, Option<String>>,
 }
 
 
@@ -23,10 +23,13 @@ pub fn extract_tag_name(html: &str) -> Tag {
     let mut tag_name: String = tag_name
         .replace("\n\r", " ")
         .replace("\n", " ")
-        .replace("\r", "");
+        .replace("\r", " ");
 
-    let id_str = find_id(tag_name.as_str());
-    let classes = find_classes(tag_name.as_str());
+    let mut attributes = find_attributes(tag_name.as_str());
+
+    let mut id_str = attributes.get("id").unwrap();
+    let mut classes = attributes.get("class").unwrap();
+
 
     // remove spaces
     end = tag_name.find(" ").unwrap_or(tag_name.len());
@@ -35,10 +38,139 @@ pub fn extract_tag_name(html: &str) -> Tag {
 
     Tag {
         name : tag_name,
-        id: id_str,
-        classes: classes,
-        attributes: None,
+        id: *id_str.clone(),
+        classes: *classes.clone(),
+        attributes: attributes,
     }
+}
+
+#[derive(PartialEq)]
+enum ReadingAttrbuteState {
+    READING_NAME,
+    READING_VALUE,
+    READING_EQUAL,
+    READING_SPACE,
+    STORE_VALUE,
+}
+
+struct Parser {
+    attribute_name: String,
+    attribute_value: String,
+    reading_state : ReadingAttrbuteState,
+    attributes: HashMap<String, Option<String>>,
+}
+
+
+fn find_attributes(tag: &str) -> HashMap<String, Option<String>> {
+
+    //let mut map = HashMap::new();
+
+    let mut attribute_value_starter : Option<char> = None;
+
+    let init_parser = Parser {
+        attribute_name: String::new(),
+        attribute_value: String::new(),
+        reading_state: ReadingAttrbuteState::READING_SPACE,
+        attributes: HashMap::new(),
+    };
+
+
+    let parsed = 
+    tag.chars()
+        .skip_while( |c| *c == ' ')
+        .fold( init_parser, |mut parser, c| {
+
+            let mut id = Some(String::new());
+            let mut class = Some(String::new());
+
+            // TODO handle the case where the attribute value is bound between 'value'
+
+            if (c == '"' || c == '\'') && parser.reading_state == ReadingAttrbuteState::READING_VALUE {
+                // end of reading value
+                parser.reading_state = ReadingAttrbuteState::STORE_VALUE;
+            }
+            if (c == '"' || c == '\'') && (parser.reading_state == ReadingAttrbuteState::READING_EQUAL || parser.reading_state == ReadingAttrbuteState::READING_SPACE) {
+                // we start reading value
+                parser.reading_state == ReadingAttrbuteState::READING_VALUE;
+                attribute_value_starter = Some('"');
+            }
+            else if c == '"' && parser.reading_state == ReadingAttrbuteState::READING_VALUE {
+                // end of reading value
+                // TODO FIXME
+            }
+            else if  c == '=' {
+                parser.reading_state = ReadingAttrbuteState::READING_EQUAL;
+            } else if c == ' ' || parser.reading_state == ReadingAttrbuteState::STORE_VALUE {
+                parser.reading_state = ReadingAttrbuteState::READING_SPACE;
+                // assigning name & values
+                parser.attributes.insert(parser.attribute_name.clone(), Some(parser.attribute_value.clone()));
+                parser.attribute_name = String::new();
+                parser.attribute_value = String::new();
+
+            } else if parser.reading_state == ReadingAttrbuteState::READING_SPACE {
+                parser.attribute_name.push(c);
+            } else if parser.reading_state == ReadingAttrbuteState::READING_EQUAL {
+                parser.reading_state = ReadingAttrbuteState::READING_VALUE;
+            } else if parser.reading_state == ReadingAttrbuteState::READING_VALUE {
+                parser.attribute_value.push(c);
+            }
+
+            parser
+
+        });
+
+
+/*
+
+    tag.split(" ")
+        .skip(1)
+        .for_each( |parts| {
+            let mut iter = parts.split("=");
+            let name = iter.next().unwrap();
+            let value = iter.next()
+                .map(|val| val.get(1..val.len()-1).unwrap().to_string());
+            if name == "id" {
+                id = value;
+            } else if name == "class" {
+                class = value;
+            } else {
+                map.insert( name.to_string(), value);
+            }
+        });
+*/
+    /* if !map.is_empty() {
+        acc.2 = Some(map);
+    } */
+
+    parsed.attributes
+
+}
+ #[cfg(test)]
+mod test_read_attributes {
+
+    use super::*;
+
+    #[test]
+    fn should_parse_attributes() {
+        let html = "div";
+        let attributes = find_attributes(html);
+        let id = attributes.get("id").unwrap();
+        let class = attributes.get("class").unwrap();
+        assert!(id.is_none());
+        assert!(class.is_none());
+    }
+
+    #[test]
+    fn should_parse_id_attributes() {
+        let html = " id='bar'";
+        let attributes = find_attributes(html);
+        let id = attributes.get("id").unwrap();
+        let class = attributes.get("class").unwrap();
+        assert_eq!(Some(String::from("bar")), *id );
+        assert!(class.is_none());
+    }
+
+
 }
 
 fn find_id(tag : &str) -> Option<String> {
@@ -70,6 +202,14 @@ fn find_classes(tag : &str) -> Option<Vec<String>> {
     }
 
     None
+}
+
+fn parse_classes(classes_str: &str) -> Option<Vec<String>> {
+    let classes : Vec<String> = classes_str.split(" ")
+        .filter( |class| !class.is_empty())
+        .map( |class| class.to_string())
+        .collect();
+    Some(classes)
 }
 
 
@@ -115,7 +255,9 @@ mod tests {
     fn should_extract_many_html_classes() {
         let html = "<div  class='bar   baz   foo mun' >";
         let tag = extract_tag_name(html);
+        assert_eq!("div", tag.name);
         assert!(tag.classes.is_some());
+        println!("=====> classes {:?}", tag.classes);
         assert_eq!(4, tag.classes.as_ref().unwrap().len());
         assert_eq!(Some(
             vec![
@@ -126,6 +268,31 @@ mod tests {
                 ]), 
             tag.classes);
     }
+
+
+    #[test]
+    fn should_extract_consider_id_and_class_as_attributes() {
+        let html = "<div id='foo' class='bar'>";
+        let tag = extract_tag_name(html);
+        assert!(tag.attributes.is_none());
+        
+        assert_eq!("foo", tag.id.unwrap());
+
+        assert_eq!("bar", tag.classes.unwrap().iter().next().unwrap() );
+    }
+    
+    #[test]
+    fn should_extract_ignore_id_and_class_as_attributes_and_read_one() {
+        let html = "<input id='foo' class='bar' type='password'>";
+        let tag = extract_tag_name(html);
+        assert!(tag.attributes.is_some());
+        let attributes = tag.attributes.unwrap();
+        println!("attributes {:?}", attributes);
+        assert_eq!(1, attributes.len());
+
+
+    }
+
     #[test]
     fn should_extract_tag_name_from_tag_into_multi_line() {
         let html = r#"<div
