c18835fbe901d292395922a6c573bd5cd8e447ed
Corentin Jechoux <corentinway@gmail.com>
2021-06-09 01:43:43 +0200

    add predicates aboout attributes

--- src/css_selector_macro.rs
+++ src/css_selector_macro.rs
@@ -30,6 +30,33 @@ macro_rules! css_selector {
             crate::selector_predicates::has_attribute_predicate(String::from(stringify!($attribute_name))),
         ])
     };
+    ($tag_name:tt [$attribute_name:tt = $attribute_value:literal ]) => {
+        crate::selector_predicates::and_predicate(vec![
+            crate::selector_predicates::tag_name_predicate(String::from(stringify!($tag_name))),
+            crate::selector_predicates::attribute_equals_predicate(
+                String::from(stringify!($attribute_name)), 
+                String::from($attribute_value)
+            ),
+        ])
+    };
+    ($tag_name:tt [$attribute_name:tt ^= $attribute_value:literal ]) => {
+        crate::selector_predicates::and_predicate(vec![
+            crate::selector_predicates::tag_name_predicate(String::from(stringify!($tag_name))),
+            crate::selector_predicates::attribute_starts_with_predicate(
+                String::from(stringify!($attribute_name)), 
+                String::from($attribute_value)
+            ),
+        ])
+    };
+    ($tag_name:tt [$attribute_name:tt $= $attribute_value:literal ]) => {
+        crate::selector_predicates::and_predicate(vec![
+            crate::selector_predicates::tag_name_predicate(String::from(stringify!($tag_name))),
+            crate::selector_predicates::attribute_ends_with_predicate(
+                String::from(stringify!($attribute_name)), 
+                String::from($attribute_value)
+            ),
+        ])
+    };
 }
 
 #[cfg(test)]
@@ -119,6 +146,73 @@ mod test_css_selector_macro {
         };
 
         let matcher = css_selector!(div[class]);
+
+        assert!(matcher(&matched_tag));
+        assert!(!matcher(&unmatched_tag));
+    }
+    #[test]
+    fn should_test_macro_given_tag_name_and_attribute_equals_vallue() {
+        let mut map = HashMap::new();
+        map.insert(String::from("class"), String::from("foo"));
+        let matched_tag = Tag {
+            name: "div".to_string(),
+            attributes: map,
+            is_autoclosing: false,
+            length: 0, // FAKE
+        };
+        let unmatched_tag = Tag {
+            name: "h1".to_string(),
+            attributes: HashMap::new(),
+            is_autoclosing: false,
+            length: 0, // FAKE
+        };
+
+        let matcher = css_selector!(div[class="foo"]);
+
+        assert!(matcher(&matched_tag));
+        assert!(!matcher(&unmatched_tag));
+    }
+    #[test]
+    fn should_test_macro_given_tag_name_and_attribute_starts_with_vallue() {
+        let mut map = HashMap::new();
+        map.insert(String::from("class"), String::from("foo"));
+        let matched_tag = Tag {
+            name: "div".to_string(),
+            attributes: map,
+            is_autoclosing: false,
+            length: 0, // FAKE
+        };
+        let unmatched_tag = Tag {
+            name: "h1".to_string(),
+            attributes: HashMap::new(),
+            is_autoclosing: false,
+            length: 0, // FAKE
+        };
+
+        let matcher = css_selector!(div[class^="foo"]);
+
+        assert!(matcher(&matched_tag));
+        assert!(!matcher(&unmatched_tag));
+    }
+    
+    #[test]
+    fn should_test_macro_given_tag_name_and_attribute_ends_with_vallue() {
+        let mut map = HashMap::new();
+        map.insert(String::from("class"), String::from("foo"));
+        let matched_tag = Tag {
+            name: "div".to_string(),
+            attributes: map,
+            is_autoclosing: false,
+            length: 0, // FAKE
+        };
+        let unmatched_tag = Tag {
+            name: "h1".to_string(),
+            attributes: HashMap::new(),
+            is_autoclosing: false,
+            length: 0, // FAKE
+        };
+
+        let matcher = css_selector!(div[class$="foo"]);
 
         assert!(matcher(&matched_tag));
         assert!(!matcher(&unmatched_tag));
--- src/selector_predicates.rs
+++ src/selector_predicates.rs
@@ -28,6 +28,52 @@ pub fn has_attribute_predicate(attribute_name: String) -> Box<dyn Fn(&Tag) -> bo
     })
 }
 
+pub fn attribute_equals_predicate(attribute_name: String, attribute_value: String) -> Box<dyn Fn(&Tag) -> bool> {
+    Box::new(move |tag: &Tag| {
+        if let Some(actual_attribute_value) = tag.attributes.get(&attribute_name) {
+            return actual_attribute_value.eq(&attribute_value);
+        }
+        false
+    })
+}
+
+pub fn attribute_starts_with_predicate(attribute_name: String, attribute_value: String) -> Box<dyn Fn(&Tag) -> bool> {
+    Box::new(move |tag: &Tag| {
+        if let Some(actual_attribute_value) = tag.attributes.get(&attribute_name) {
+            return actual_attribute_value.starts_with(&attribute_value);
+        }
+        false
+    })
+}
+
+pub fn attribute_ends_with_predicate(attribute_name: String, attribute_value: String) -> Box<dyn Fn(&Tag) -> bool> {
+    Box::new(move |tag: &Tag| {
+        if let Some(actual_attribute_value) = tag.attributes.get(&attribute_name) {
+            return actual_attribute_value.ends_with(&attribute_value);
+        }
+        false
+    })
+}
+
+pub fn attribute_contains_with_predicate(attribute_name: String, attribute_value: String) -> Box<dyn Fn(&Tag) -> bool> {
+    Box::new(move |tag: &Tag| {
+        if let Some(actual_attribute_value) = tag.attributes.get(&attribute_name) {
+            return actual_attribute_value.contains(&attribute_value);
+        }
+        false
+    })
+}
+pub fn attribute_has_word_predicate(attribute_name: String, word: String) -> Box<dyn Fn(&Tag) -> bool> {
+    Box::new(move |tag: &Tag| {
+        if let Some(actual_attribute_value) = tag.attributes.get(&attribute_name) {
+            return actual_attribute_value.starts_with(&format!("{} ", &word)) ||
+            actual_attribute_value.contains(&format!(" {} ", &word)) ||
+            actual_attribute_value.ends_with(&format!(" {}", &word));
+        }
+        false
+    })
+}
+
 pub fn and_predicate(predicates: Vec<Box<dyn Fn(&Tag) -> bool>>) -> Box<dyn Fn(&Tag) -> bool> {
     Box::new(move |tag: &Tag| {
         predicates
@@ -128,4 +174,107 @@ mod test_selectors {
 
         assert!(does_match);
     }
+    #[test]
+    fn should_match_a_tag_with_attribute_equals_its_value() {
+        let mut map = HashMap::new();
+        map.insert("foo".to_string(), "bar".to_string());
+        let tag = Tag {
+            name: String::from("div"),
+            attributes: map,
+            length: 5, // FAKE
+            is_autoclosing: false,
+        };
+
+        let matcher = attribute_equals_predicate(String::from("foo"), String::from("bar"));
+
+        let does_match = matcher(&tag);
+
+        assert!(does_match);
+    }
+    #[test]
+    fn should_match_a_tag_with_attribute_starts_with_its_value() {
+        let mut map = HashMap::new();
+        map.insert("foo".to_string(), "baaaaar".to_string());
+        let tag = Tag {
+            name: String::from("div"),
+            attributes: map,
+            length: 5, // FAKE
+            is_autoclosing: false,
+        };
+
+        let matcher = attribute_starts_with_predicate(String::from("foo"), String::from("baaa"));
+
+        let does_match = matcher(&tag);
+
+        assert!(does_match);
+    }
+    #[test]
+    fn should_match_a_tag_with_attribute_ends_with_its_value() {
+        let mut map = HashMap::new();
+        map.insert("foo".to_string(), "baaaaar".to_string());
+        let tag = Tag {
+            name: String::from("div"),
+            attributes: map,
+            length: 5, // FAKE
+            is_autoclosing: false,
+        };
+
+        let matcher = attribute_ends_with_predicate(String::from("foo"), String::from("aar"));
+
+        let does_match = matcher(&tag);
+
+        assert!(does_match);
+    }
+    #[test]
+    fn should_match_a_tag_with_attribute_contains_with_its_value() {
+        let mut map = HashMap::new();
+        map.insert("foo".to_string(), "baaaaar".to_string());
+        let tag = Tag {
+            name: String::from("div"),
+            attributes: map,
+            length: 5, // FAKE
+            is_autoclosing: false,
+        };
+
+        let matcher = attribute_contains_with_predicate(String::from("foo"), String::from("aaa"));
+
+        let does_match = matcher(&tag);
+
+        assert!(does_match);
+    }
+    #[test]
+    fn should_match_a_tag_with_attribute_has_a_word_with_its_value() {
+        let mut map = HashMap::new();
+        map.insert("data".to_string(), "foo bar baz".to_string());
+        let tag = Tag {
+            name: String::from("div"),
+            attributes: map,
+            length: 5, // FAKE
+            is_autoclosing: false,
+        };
+
+        let matcher = attribute_has_word_predicate(String::from("data"), String::from("foo"));
+        let does_match = matcher(&tag);
+        assert!(does_match);
+        let matcher = attribute_has_word_predicate(String::from("data"), String::from("bar"));
+        let does_match = matcher(&tag);
+        assert!(does_match);
+        let matcher = attribute_has_word_predicate(String::from("data"), String::from("baz"));
+        let does_match = matcher(&tag);
+        assert!(does_match);
+    }
+
+
+    // X:not(selector)
+    // X::pseudoElement
+    // X:nth-child(n)    
+    // X:nth-last-child(n)
+    // X:nth-of-type(n)
+    // X:nth-last-of-type(n)
+    // X:first-child
+    // X:last-child
+    // X:only-child
+    // X:only-of-type
+    // X:first-of-type
+
 }
